f045a18f8ab94466f2cd20afdded409015ccad20
pascal.betting <pascal.betting@intive.com>
2018-08-07 19:54:14 +0200

    Add correct project

---
+++ Test Suites/Regression.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3bbe2d21-9d6d-41d3-b24f-15e8ab2e42b2</testSuiteGuid>
+   <testCaseLink>
+      <guid>a0c36bcd-1348-49f5-ab36-6522b0561b43</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase A</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
